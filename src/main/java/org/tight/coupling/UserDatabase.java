16d74238cdc37a19ce8e66f31354a374f38e7e80
TuanKiet97 <tuankiet.thptcamau@gmail.com>
2024-09-29 19:31:56 +0700

    Hands On Tight Coupling

---
+++ src/main/java/org/tight/coupling/UserDatabase.java
@@ -0,0 +1,8 @@
+package org.tight.coupling;
+
+public class UserDatabase {
+    public String getUserDetails () {
+        // Directly access database
+        return "User from database";
+    }
+}
