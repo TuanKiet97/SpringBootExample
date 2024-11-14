c820b176cb023b3caabca594cd093ad7de585cda
TuanKiet97 <tuankiet.thptcamau@gmail.com>
2024-11-15 00:33:11 +0700

    Handle ResponseEntity

--- ecom-project/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ ecom-project/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,7 +1,10 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,10 +28,10 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public String deleteCategory(Long categoryId) {
         Category category = categories.stream()
-                .filter(c->c.getCategoryId().equals(categoryId)).findFirst().orElse(null);
-        if (category == null) {
-            return "Category not found !";
-        }
+                .filter(c->c.getCategoryId()
+                .equals(categoryId))
+                .findFirst()
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
         categories.remove(category);
         return "Category with CategoryId: " + categoryId + " deleted successfully";
     }
