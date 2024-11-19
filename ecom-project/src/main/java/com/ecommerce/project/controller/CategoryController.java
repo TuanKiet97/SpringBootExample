2f9ee76b3b2d721f64c33e6dc3d9a0a1210d8e6b
TuanKiet97 <tuankiet.thptcamau@gmail.com>
2024-11-19 23:37:29 +0700

    Handle update category

--- ecom-project/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ ecom-project/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -39,4 +39,15 @@ public class CategoryController {
         }
 
     }
+
+    @PutMapping("/api/public/categories/{categoryId}")
+    public ResponseEntity<String> updateCategory(@RequestBody Category category,
+                                                 @PathVariable Long categoryId) {
+        try {
+            Category saveCategory = categoryService.updateCategory(category, categoryId);
+            return new ResponseEntity<>("Category with category id: " + categoryId, HttpStatus.OK);
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
+    }
 }
