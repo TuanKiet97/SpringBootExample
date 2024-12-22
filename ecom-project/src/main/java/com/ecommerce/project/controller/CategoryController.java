5e334b0ce23d38a08e6022d173a21fcd2a4beb07
TuanKiet97 <tuankiet.thptcamau@gmail.com>
2024-12-22 16:39:07 +0700

    Convert object to DTO using modelmapper

--- ecom-project/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ ecom-project/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.payload.CategoryResponse;
 import com.ecommerce.project.service.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +20,9 @@ public class CategoryController {
 
     @GetMapping("/public/categories")
     //@RequestMapping(value = "/public/categories", method = RequestMethod.GET)
-    public ResponseEntity<List<Category>> getAllCategories(){
-        List<Category> categories = categoryService.getAllCategories();
-        return new ResponseEntity<>(categories, HttpStatus.OK);
+    public ResponseEntity<CategoryResponse> getAllCategories(){
+        CategoryResponse categoryResponse = categoryService.getAllCategories();
+        return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
 
     @PostMapping("/public/categories")
@@ -39,7 +40,7 @@ public class CategoryController {
 
 
     @PutMapping("/public/categories/{categoryId}")
-    public ResponseEntity<String> updateCategory(@Valid Va@RequestBody Category category,
+    public ResponseEntity<String> updateCategory(@Valid @RequestBody Category category,
                                                  @PathVariable Long categoryId){
             Category savedCategory = categoryService.updateCategory(category, categoryId);
             return new ResponseEntity<>("Category updated with category id: " + categoryId, HttpStatus.OK);
